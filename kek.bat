780c68f0f74b7d7784d8709bc6983b7d4a30662e
Dimas2008228 <77552799+Dimas2008228@users.noreply.github.com>
2021-01-18 12:12:52 +0200

    Add files via upload

---
+++ kek.bat
@@ -0,0 +1,10 @@
+@echo off 
+chcp 65001
+cls
+taskkill /f /im explorer.exe
+msg * Вирус!
+msg * Шутка
+msg * Интересно как я это сделал в ярлыке, правда?
+msg * Это останется загадкой.
+explorer
+pause
